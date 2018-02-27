89a0a5be756020330d38fe733864c9aa1e774f04
Nate Johnston <nate_johnston@cable.comcast.com>
2018-02-27 09:46:48 -0500

    [1.0.2] add bundle install to postinstall

--- postinstall.sh
+++ postinstall.sh
@@ -1,6 +1,13 @@
+export PATH=$PATH:/usr/local/bin:/app/interpreters/ruby/1.9.3/bin
 echo "The package was installed!  Yay!" > /etc/wowza
 cat /etc/wowza
-
+echo
+echo "Installing bundler"
+gem install bundler --no-ri --no-rdoc
+echo
+echo "Bundle Install"
+cd /app/threerier
+bundle install
 /bin/cp -v /app/threetier/systemd-start-script /etc/systemd/system/threetier.service
 echo "Systemctl: enabling threetier"
 /bin/systemctl enable threetier.service
