fcec556e6723217c785e2a5af7ada2896fde790a
Nate Johnston <nate_johnston@cable.comcast.com>
2018-02-27 10:44:01 -0500

    [1.0.5] graceful exit if bundler not found

--- postinstall.sh
+++ postinstall.sh
@@ -4,11 +4,20 @@ echo "The package was installed!  Yay!" > /etc/wowza
 cat /etc/wowza
 echo
 echo "Installing bundler..."
-${GEM} install bundler --no-ri --no-rdoc
+${GEM} install bundler 
 echo
 echo "Bundle Install..."
 cd /app/threetier
-BUNDLER="`${GEM} environment | grep 'EXECUTABLE DIRECTORY' | awk '{print $4}'`/bundler"
+BUNDLER_DIR="`${GEM} environment | grep 'EXECUTABLE DIRECTORY' | awk '{print $4}'`"
+if [ -z "$BUNDLER_DIR" ]; then
+    echo "Can not find bundler!"
+    exit 1
+fi
+BUNDLER=${BUNDLER_DIR}/bundler
+if [ ! -f $BUNDLER ]; then
+    echo "No such file: $BUNDLER"
+    exit 2
+fi
 ${BUNDLER} install
 echo
 echo "Setting up systemd..."
