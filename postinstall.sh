a652ae42eb087534a51d9ba433b4d57519dd0097
Nate Johnston <nate_johnston@cable.comcast.com>
2018-04-09 13:46:15 -0400

    [1.0.39] remove ruby from postinstall

--- postinstall.sh
+++ postinstall.sh
@@ -2,34 +2,36 @@ export PATH=$PATH:/usr/local/bin:/app/interpreters/ruby/1.9.3/bin
 apt-get install -y golang-go
 echo "The package was installed!  Yay!" > /etc/wowza
 cat /etc/wowza
-echo "DEBUG: List all files installed by ruby package"
-/usr/bin/dpkg -L ruby
-echo
-GEM=`/usr/bin/dpkg -L ruby | grep bin/gem | head -1`
-echo "Install of bundler running (${GEM} install bundler)"
-if [ -z "$GEM" ]; then
-    echo "Can not find gem!"
-    exit 1
-fi
-if [ ! -f $GEM ]; then
-    echo "No such file: $GEM"
-    exit 2
-fi
-${GEM} install bundler 
-echo
-cd /app/threetier
-BUNDLER_DIR="`${GEM} environment | grep 'EXECUTABLE DIRECTORY' | awk '{print $4}'`"
-if [ -z "$BUNDLER_DIR" ]; then
-    echo "Can not find bundler!"
-    exit 1
-fi
-BUNDLER=${BUNDLER_DIR}/bundler
-if [ ! -f $BUNDLER ]; then
-    echo "No such file: $BUNDLER"
-    exit 2
-fi
-echo "Bundle Install running (${BUNDLER} install)"
-${BUNDLER} install
+
+#echo "DEBUG: List all files installed by ruby package"
+#/usr/bin/dpkg -L ruby
+#echo
+#GEM=`/usr/bin/dpkg -L ruby | grep bin/gem | head -1`
+#echo "Install of bundler running (${GEM} install bundler)"
+#if [ -z "$GEM" ]; then
+#    echo "Can not find gem!"
+#    exit 1
+#fi
+#if [ ! -f $GEM ]; then
+#    echo "No such file: $GEM"
+#    exit 2
+#fi
+#${GEM} install bundler 
+#echo
+#cd /app/threetier
+#BUNDLER_DIR="`${GEM} environment | grep 'EXECUTABLE DIRECTORY' | awk '{print $4}'`"
+#if [ -z "$BUNDLER_DIR" ]; then
+#    echo "Can not find bundler!"
+#    exit 1
+#fi
+#BUNDLER=${BUNDLER_DIR}/bundler
+#if [ ! -f $BUNDLER ]; then
+#    echo "No such file: $BUNDLER"
+#    exit 2
+#fi
+#echo "Bundle Install running (${BUNDLER} install)"
+#${BUNDLER} install
+
 echo
 echo "Setting up systemd..."
 /bin/cp -v /app/threetier/systemd-start-script /etc/systemd/system/threetier.service
